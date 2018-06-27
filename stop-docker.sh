4d45ecf4afc34ee918073aa4ee9ee79ff0cdb735
Vir Desai <vir.desai.nc@gmail.com>
2018-06-26 23:59:51 -0400

    rpi 3 b+ test

--- stop-docker.sh
+++ stop-docker.sh
@@ -3,5 +3,5 @@ docker rm $(docker ps -aq)
 docker rmi $(docker images -q)
 docker volume rm $(docker volume ls -q)
 docker network prune
-docker-compose -f docker/docker-compose.yml down -vt5
+docker-compose -f docker/docker-compose.yml down --rmi all -v --remove-orphans
 docker-compose -f docker/docker-compose.yml rm -fv
